55c46fe4d367adcd96e4979b67c0b76c33b0f8ea
FranciscoMaik <chiconunes3081@gmail.com>
2023-08-14 12:18:15 -0300

    feat: create first template

---
+++ src/functions/useForm.ts
@@ -0,0 +1,91 @@
+import { FormEvent, useMemo, useState } from 'react';
+import uuid from 'react-uuid';
+
+export type TypeItem = 'feat' | 'fix' | 'style' | 'refactor' | 'test' | 'chore';
+
+interface ItemsProps {
+  id: string;
+  description: string;
+  type: TypeItem;
+}
+
+interface PRProps {
+  id: string;
+  description: string;
+}
+
+export const useForm = () => {
+  const [items, setItems] = useState<Array<ItemsProps>>([]);
+  const [itemsAdd, setItemsAdd] = useState<ItemsProps>({
+    id: '',
+    description: '',
+    type: 'feat',
+  } as ItemsProps);
+  const [pullRequest, setPullRequest] = useState<Array<PRProps>>([]);
+  const [pullRequestAdd, setPullRequestAdd] = useState<PRProps>({
+    description: '',
+    id: '',
+  } as PRProps);
+
+  const addItem = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setItems(prevState => [...prevState, { ...itemsAdd, id: uuid() }]);
+  };
+
+  const addPullRequest = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setPullRequest(prevState => [
+      ...prevState,
+      { ...pullRequestAdd, id: uuid() },
+    ]);
+  };
+
+  const filterItemsForType = (type: TypeItem) =>
+    items
+      .filter(item => item.type === type)
+      .map((item, index) => `${index + 1}. ${item.description}`)
+      .join('\n');
+
+  const generateMarkdown = useMemo(() => {
+    const markdownPR = pullRequest
+      .map((item, index) => `${index + 1}. #${item.description}`)
+      .join('\n');
+
+    const markdown = `
+      ## Descrição
+      Nesta _branch_ contém as seguintes alterações:
+
+      ### Novas funcionalidades
+      ${filterItemsForType('feat')}
+
+      ### Correções
+      ${filterItemsForType('fix')}
+
+      ### Estilos
+      ${filterItemsForType('style')}
+
+      ### Refatorações
+      ${filterItemsForType('refactor')}
+
+      ### Testes
+      ${filterItemsForType('test')}
+
+      ### Mudanças em arquivos
+      ${filterItemsForType('chore')}
+
+      ## Pull Requests
+      ${markdownPR}`;
+
+    return markdown;
+  }, [items, pullRequest]);
+
+  return {
+    addItem,
+    addPullRequest,
+    generateMarkdown,
+    itemsAdd,
+    setItemsAdd,
+    pullRequestAdd,
+    setPullRequestAdd,
+  };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { useForm, TypeItem } from '../functions/useForm';
 
 const Home: React.FC = function () {
+  const {
+    addItem,
+    addPullRequest,
+    generateMarkdown,
+    itemsAdd,
+    pullRequestAdd,
+    setItemsAdd,
+    setPullRequestAdd,
+  } = useForm();
+
   return (
     <div>
-      <p>Home</p>
+      <h4>Items</h4>
+      <form onSubmit={addItem}>
+        <p>Descrição</p>
+        <input
+          type="text"
+          value={itemsAdd?.description}
+          onChange={value =>
+            setItemsAdd({ ...itemsAdd, description: value.target.value })
+          }
+        />
+        <p>Tipo</p>
+        <select
+          id="frutas"
+          name="frutas"
+          value={itemsAdd.type}
+          onChange={value =>
+            setItemsAdd({ ...itemsAdd, type: value.target.value as TypeItem })
+          }
+        >
+          <option value="feat">Feat</option>
+          <option value="fix">Fix</option>
+          <option value="style">Style</option>
+          <option value="refactor">Refactor</option>
+          <option value="test">Test</option>
+          <option value="chore">Chore</option>
+        </select>
+
+        <button type="submit">Adicionar</button>
+      </form>
+
+      <h4>PRs</h4>
+      <form onSubmit={addPullRequest}>
+        <p>Número da PR</p>
+        <input
+          type="text"
+          value={pullRequestAdd?.description}
+          onChange={value =>
+            setPullRequestAdd({
+              ...pullRequestAdd,
+              description: value.target.value,
+            })
+          }
+        />
+
+        <button type="submit">Adicionar</button>
+      </form>
+
+      <h4>Resultado</h4>
+
+      <ReactMarkdown>{generateMarkdown}</ReactMarkdown>
     </div>
   );
 };
